f69dcc98d125684bb34180dd0fee5eaabdc3cf4b
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-19 23:25:43 +0530

    Feat: Dashboard

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -1,45 +1,63 @@
-import { useJobs } from "../hooks/useJobs"
-import { formatDate } from "../utils/formatDate"
+import { useJobs } from "../hooks/useJobs";
+import { formatDate } from "../utils/formatDate";
 import { useState } from "react";
 import ModalEdit from "./ModalEdit";
 
+const JobCard = ({ job }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [editingJob, setEditingJob] = useState(null);
 
-const JobCard = ({job}) => {
-const [isOpen, setIsOpen] = useState(false);
-    const [editingJob, setEditingJob] = useState(null);
+  const { dispatch } = useJobs();
 
-    const {dispatch} = useJobs()
+  function handleDelete(id) {
+    dispatch({
+      type: "DELETE_JOB",
+      payload: id,
+    });
+  }
 
-    function handleDelete(id){
-        dispatch({
-            type: 'DELETE_JOB',
-            payload: id
-        })
-    }
-
-    function handleEdit(){
-        setIsOpen(true);
-        setEditingJob(job)
-    }
+  function handleEdit() {
+    setIsOpen(true);
+    setEditingJob(job);
+  }
 
   return (
     <>
-    <div className="border border-zinc-300 px-2 py-3 rounded-md flex flex-col gap-2">
+      <div className="border border-zinc-300 px-2 py-3 rounded-md flex flex-col gap-2">
         <div className="flex flex-col">
-            <h2 className="text-2xl font-semibold">{job.companyName}</h2>
-            <h3 className="text-slate-800 mb-2">{job.jobRole}</h3>
-            <p className="border rounded-full w-fit px-4 py-0.5 text-xs">{job.status}</p>
-            <p className="text-xs">{formatDate(job.date)}</p>
-        </div>  
-        <div className="flex items-center w-full gap-1 mt-auto">
-            <button onClick={handleEdit} className="border text-sky-500 font-medium bg-sky-100 border-sky-400 rounded-lg px-2 py-1 cursor-pointer flex-1">Edit</button>
-            <button onClick={()=>handleDelete(job.id)} className="border border-red-400 bg-red-100 text-red-500 font-medium rounded-lg px-2 py-1 cursor-pointer flex-1">Delete</button>
+          <h2 className="text-2xl font-semibold">{job.companyName}</h2>
+          <h3 className="text-slate-800 mb-2">{job.jobRole}</h3>
+          <p className="border rounded-full w-fit px-4 py-0.5 text-xs">
+            {job.status}
+          </p>
+          <p className="text-xs">{formatDate(job.date)}</p>
         </div>
-    </div>
+        <div className="flex items-center w-full gap-1 mt-auto">
+          <button
+            onClick={handleEdit}
+            className="border text-sky-500 font-medium bg-sky-100 border-sky-400 rounded-lg px-2 py-1 cursor-pointer flex-1"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => handleDelete(job.id)}
+            className="border border-red-400 bg-red-100 text-red-500 font-medium rounded-lg px-2 py-1 cursor-pointer flex-1"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
 
-    {isOpen && <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen} setEditingJob={setEditingJob} editingJob={editingJob}/>}
+      {isOpen && (
+        <ModalEdit
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          setEditingJob={setEditingJob}
+          editingJob={editingJob}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default JobCard
+export default JobCard;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,44 @@
+import Container from "../components/Container"
+import { useJobs } from "../hooks/useJobs"
 
 const Dashboard = () => {
+  const {jobs} = useJobs();
+
   return (
-    <div>Dashboard</div>
+    <section className="min-h-screen bg-slate-100 py-8">
+      <Container>
+        <div>
+
+          {/* stats */}
+          <div className="flex rounded-xl justify-between gap-2 p-2">
+            {/* applied */}
+            <div className="border border-zinc-300 p-1 rounded-md flex flex-col gap-2 items-center justify-center">
+              <p className="">Applied</p>
+              <p className="text-2xl">
+              {jobs.filter(job => job.status === 'Applied').length}
+              </p>
+            </div>
+
+            {/* interview */}
+            <div className="border border-zinc-300 p-1.5 rounded-md flex flex-col gap-2 items-center justify-center">
+              <p className="">Interview</p>
+              <p className="text-2xl">
+              {jobs.filter(job => job.status === 'Interviewing').length}
+              </p>
+            </div>
+
+            {/* reject */}
+            <div className="border border-zinc-300 p-1.5 rounded-md flex flex-col gap-2 items-center justify-center">
+              <p className="">Rejected</p>
+              <p className="text-2xl">
+              {jobs.filter(job => job.status === 'Rejected').length}
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
