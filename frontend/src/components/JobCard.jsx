1042484ee6c1f59e671bffea710e6661c2a51990
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-20 13:58:00 +0530

    Update: job card

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -23,15 +23,20 @@ const JobCard = ({ job }) => {
 
   return (
     <>
-      <div className="border border-zinc-300 px-2 py-3 rounded-md flex flex-col gap-2">
-        <div className="flex flex-col">
-          <h2 className="text-2xl font-semibold">{job.companyName}</h2>
-          <h3 className="text-slate-800 mb-2">{job.jobRole}</h3>
-          <p className="border rounded-full w-fit px-4 py-0.5 text-xs">
+      <div className="relative border border-zinc-300 px-2 py-3 rounded-md flex flex-col gap-2">
+        <p className="absolute top-3 right-2 border rounded-full w-fit px-4 py-0.5 text-xs">
             {job.status}
           </p>
+        <div className="flex flex-col">
+          <div className="bg-slate-300 h-12 w-12 text-2xl flex justify-center items-center font-semibold rounded-md">
+            {job.companyName.at(0).toUpperCase()}
+          </div>
+          <h2 className="text-2xl font-semibold mt-1">{job.companyName}</h2>
+          <h3 className="text-slate-800 mb-2 -mt-1">{job.jobRole}</h3>
           <p className="text-xs">{formatDate(job.date)}</p>
         </div>
+
+        <hr className="my-2 text-zinc-300"/>
         <div className="flex items-center w-full gap-1 mt-auto">
           <button
             onClick={handleEdit}
