82c73fe86a16bca94a8c0479daaf4cb76514ea8f
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-22 13:48:26 +0530

    Feat: GET api

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -10,3 +10,14 @@ export const createJob = async (req, res) => {
         })
     }
 }
+
+export const getJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find().sort({createdAt: -1});
+        res.status(200).json(jobs);
+    }catch(err){
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+}
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createJob } from '../controllers/jobController.js';
+import { createJob, getJobs } from '../controllers/jobController.js';
 
 const router = express.Router();
 
 // Post /api/jobs
-
 router.post('/', createJob);
+
+// GET
+router.get('/', getJobs);
 
 export default router;
 
