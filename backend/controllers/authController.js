eaf2e48e65378c4ca0cea844e5d7ede716d2099b
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-27 22:06:23 +0530

    Feat: Login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import { User } from "../models/User";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
     try {
@@ -34,3 +35,44 @@ export const registerUser = async (req, res) => {
         })
     }
 };
+
+export const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        // compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        //create token
+        const token = jwt.sign({
+            id: user._id
+        }, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    }catch(err){
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
